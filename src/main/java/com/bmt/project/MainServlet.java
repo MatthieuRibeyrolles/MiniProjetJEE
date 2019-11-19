6c5630e97bbab7c950b62787f037a57c41f4da66
bruno <brunopierre.mzl@gmail.com>
2019-11-19 14:52:24 +0100

    Merge origin/master

--- src/main/java/com/bmt/project/MainServlet.java
+++ src/main/java/com/bmt/project/MainServlet.java
@@ -5,8 +5,13 @@
  */
 package com.bmt.project;
 
+import com.bmt.project.model.DAO;
+import com.bmt.project.model.DataSourceFactory;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,5 +28,102 @@ public class MainServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("WEB-INF/main").forward(request, response);
+            
+        
+        try (PrintWriter out = response.getWriter()) {
+            
+            //DAO MyDao = new DAO(DataSourceFactory.getDataSource());
+            
+            
+//connection a la base de donnée//   
+            try {
+                String log = request.getParameter("login");
+                String pass = request.getParameter("password");
+               
+                if (log == null | pass == null) {
+                    throw new Exception("le login ou le password sont null");
+                }
+                
+                //MyDa0.Login(log,pass);
+
+                
+
+            }catch (Exception e) {
+                out.printf("Erreur : %s", e.getMessage());
+            }
+///////////////////////////////////
+
+
+
+
+// modifier les informations personnelles//
+            try {
+                ArrayList<String> infoPerso = new ArrayList<String>();
+                
+                String contact = request.getParameter("contact");
+                infoPerso.add(contact);
+                String fonction = request.getParameter("fonction");
+                infoPerso.add(fonction);
+                String adresse = request.getParameter("adresse");
+                infoPerso.add(adresse);
+                String ville = request.getParameter("ville");
+                infoPerso.add(ville);
+                String region = request.getParameter("region");
+                infoPerso.add(region);
+                String code_postal = request.getParameter("code_postal");
+                infoPerso.add(code_postal);
+                String pays = request.getParameter("pays");
+                infoPerso.add(pays);
+                String telephone = request.getParameter("telephone");
+                infoPerso.add(telephone);
+                String fax = request.getParameter("fax");
+                infoPerso.add(fax);
+                
+                Boolean flag = true;
+                for (int i = 0 ; i < infoPerso.size() ; i++){
+                    if (infoPerso.get(i)==null)
+                        flag=false;                    
+                }
+                if (flag == false ){
+                    throw new Exception("une des informations est null");
+                }
+                //MyDao.updateProfile(infoPerso);
+                
+            }catch (Exception e) {
+                out.printf("Erreur pendant la modif de données perso");
+            }
+
+
+//ajouter des commandes//            
+            
+
+
+
+            
+///////////////////////////////////
+
+
+//editer des commandes
+
+
+
+
+///////////////////////////////////
+
+
+//supprimer des commandes 
+
+
+
+
+///////////////////////////////////
+
+
+
+
+        } catch (Exception ex) {
+            Logger.getLogger("servlet").log(Level.SEVERE, "Erreur de traitement", ex);
+        }
+        
     }
 }
