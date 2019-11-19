6ccb980dfc8a73108b10b72da157bdde7f912047
Thibault Ajas <thibault.ajas@etud.univ-jfc.fr>
2019-11-19 14:45:13 +0100

    creation premiers tests de la DAO + debut de la classe client

---
+++ src/main/java/com/bmt/project/model/OrderEntity.java
@@ -0,0 +1,13 @@
+package com.bmt.project.model;
+
+/**
+ *
+ * @author Thibault
+ */
+public class OrderEntity {
+    
+    private int num;
+    private ClientEntity client;
+    // private DATE date;
+    
+}
