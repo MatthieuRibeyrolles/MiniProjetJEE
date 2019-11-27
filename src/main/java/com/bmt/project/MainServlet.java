f836065aac3e9f6ad23fc401784407f3255e673c
bruno <brunopierre.mzl@gmail.com>
2019-11-27 13:43:03 +0100

    modif ctrl

--- src/main/java/com/bmt/project/MainServlet.java
+++ src/main/java/com/bmt/project/MainServlet.java
@@ -5,6 +5,9 @@
  */
 package com.bmt.project;
 
+import com.bmt.project.model.CategoryEntity;
+import com.bmt.project.model.DAO;
+import com.bmt.project.model.DataSourceFactory;
 import com.bmt.project.model.OrderEntity;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -13,6 +16,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -29,6 +33,17 @@ public class MainServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        DAO MyDao = new DAO(DataSourceFactory.getDataSource());
+        
+        List<CategoryEntity> cat = MyDao.getCategoriesList();
+        
+        List<String> nomcat = new ArrayList<String>();
+        for(CategoryEntity c : cat ){
+            nomcat.add(c.getWording());
+        }
+        
+        request.setAttribute("categories_list",nomcat);
+
         this.getServletContext().getRequestDispatcher("/WEB-INF/main_jsp.jsp").forward(request, response);
             
         
@@ -37,7 +52,7 @@ public class MainServlet extends HttpServlet {
             //DAO MyDao = new DAO(DataSourceFactory.getDataSource());
             
             
-//connection a la base de donnée//   
+//connection a la base de donnée depuis un client//   
             try {
                 String log = request.getParameter("login");
                 String pass = request.getParameter("password");
@@ -49,7 +64,7 @@ public class MainServlet extends HttpServlet {
                 }
                 
                 //ClientEntity clientlogged  = new MyDa0.login(log,pass);
-
+                //List<OrderEntity> listOrder = myDao.getOrdersList();
                 
 
             }catch (Exception e) {
@@ -96,25 +111,27 @@ public class MainServlet extends HttpServlet {
 
             try {
 
+                String datesend = request.getParameter("receptiondate");
 
+                String fee = request.getParameter("fee");
                 
-                Date ajd = new Date();
-                DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-                String dat = dateFormat.format(ajd);
+                String destination = request.getParameter("destination");
                 
-//////                
-//////                faut rajouter la date de livraison et je sais pas comment la 
-//////                faire (combien de jours si on peut choisir la date ou si 
-//////                la date est attribué automatiquement)
-//////                
+                String add_livraison  = request.getParameter("add_livraison");
                 
-                Random rng = new Random();
-                int temp = rng.nextInt(1_0000_00); 
-
+                String city_livraison = request.getParameter("city_livraison");
                 
-                OrderEntity neworder = new OrderEntity();   
+                String region_livraison  =request.getParameter("region_livraison");
                 
+                String zipCode = request.getParameter("zipCode");
                 
+                String country_livraison = request.getParameter("country_livraison");
+                
+                String remise = request.getParameter("remise");
+                
+                //OrderEntity neworder = new OrderEntity(clientlogged,datesend,fee,destionation,add_livrasion,city_livrasion,region_livrasion,zipCode,country_livrasion,remise);   
+                
+                //myDao.addOrder(neworder);
                 
                 
                 
@@ -134,21 +151,56 @@ public class MainServlet extends HttpServlet {
 /////////////////////////////////////
 
 
+////supprimer des commandes 
+            try{
+                String num = request.getParameter("num");
+                
+                //myDao.deleteOrder(num);
+            
+            }catch (Exception e){
+                out.printf("erreur lors de la suppression d'une commande");
+            }
+
+/////////////////////////////////////
+
 
 
 ////editer des commandes
+            try {
+                
+                String datesend = request.getParameter("receptiondate");
 
+                String fee = request.getParameter("fee");
+                
+                String destination = request.getParameter("destination");
+                
+                String add_livraison  = request.getParameter("add_livraison");
+                
+                String city_livraison = request.getParameter("city_livraison");
+                
+                String region_livraison  =request.getParameter("region_livraison");
+                
+                String zipCode = request.getParameter("zipCode");
+                
+                String country_livraison = request.getParameter("country_livraison");
+                
+                String remise = request.getParameter("remise");
+                
+                
+                
+                //OrderEntity neworder = new OrderEntity(clientlogged,datesend,fee,destionation,add_livrasion,city_livrasion,region_livrasion,zipCode,country_livrasion,remise);   
+                
+                //myDao.editOrder(oldorder,neworder);
+                
+            }catch( Exception e){
+                out.printf("erreur lors de la modification d'une commande");
+            }
 
 
 
 /////////////////////////////////////
 
-////supprimer des commandes 
 
-
-
-
-/////////////////////////////////////
 
 
 
