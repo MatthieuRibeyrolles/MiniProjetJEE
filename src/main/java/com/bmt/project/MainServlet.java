22ab4526eaa4c90f6284330eb33b4098a2c87ddb
bruno <brunopierre.mzl@gmail.com>
2019-12-04 13:24:53 +0100

    ???

--- src/main/java/com/bmt/project/MainServlet.java
+++ src/main/java/com/bmt/project/MainServlet.java
@@ -36,212 +36,158 @@ public class MainServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
+
         DAO MyDao = new DAO(DataSourceFactory.getDataSource());
-       
-        
-        
-        
-        
+
         //liste des catégories et map des produits
         List<CategoryEntity> cat = MyDao.getCategoriesList();
         List<ProductEntity> prod = MyDao.getProductsList();
-        
+
         List<String> nomcat = new ArrayList<String>();
-       
+
         Map<String, ArrayList<String>> mapProduct = new HashMap<>();
 //        List<ProductEntity> listProduct = MyDao.getProductlist();
 
-        
-        for(CategoryEntity c : cat ){
+        for (CategoryEntity c : cat) {
+
             nomcat.add(c.getWording());
-            mapProduct.put(c.getWording(),new ArrayList<String>());
+            mapProduct.put(c.getWording(), new ArrayList<String>());
+
         }
-        System.out.println(nomcat.get(0));
-       
-        
-        for(ProductEntity p : prod){
-            ArrayList<String> tmp = mapProduct.get(p.getCategory());
+
+        for (ProductEntity p : prod) {
+
+            ArrayList<String> tmp = new ArrayList<String>();
+            tmp = mapProduct.get(p.getCategory().getWording());
+
             tmp.add(p.getName());
-            mapProduct.put(p.getCategory().getWording(),tmp);
+            mapProduct.put(p.getCategory().getWording(), tmp);
         }
-        
-     
-        request.setAttribute("product_map",mapProduct);
-        request.setAttribute("categories_list",nomcat);
+
+        request.setAttribute("product_map", mapProduct);
+        request.setAttribute("categories_list", nomcat);
         //fin de la liste des catégories et de la map des produits
-        
-        
 
-
-
-
-        
-        
         this.getServletContext().getRequestDispatcher("/WEB-INF/main_jsp.jsp").forward(request, response);
-            
-        
-        try  {
-            
+
+        try {
+
             //DAO MyDao = new DAO(DataSourceFactory.getDataSource());
-            
-            
 //connection a la base de donnée depuis un client//   
             try {
                 String log = request.getParameter("login");
                 String pass = request.getParameter("password");
-                
-                
-                
+
                 if (log == null | pass == null) {
                     throw new Exception("le login ou le password sont null");
                 }
-                
+
                 //ClientEntity clientlogged  = new MyDa0.login(log,pass);
                 //List<OrderEntity> listOrder = myDao.getOrdersList();
-                
-
-            }catch (Exception e) {
+            } catch (Exception e) {
                 out.printf("Erreur : %s", e.getMessage());
             }
 ///////////////////////////////////
 
-
-
-
 // modifier les informations personnelles//
             try {
-                
+
                 //String code = clientlogged.getCode();
                 //String societe = clientlogged.getCompany();
                 String contact = request.getParameter("contact");
-               
+
                 String fonction = request.getParameter("fonction");
-               
+
                 String adresse = request.getParameter("adresse");
-                
+
                 String ville = request.getParameter("ville");
-             
+
                 String region = request.getParameter("region");
-               
+
                 String code_postal = request.getParameter("code_postal");
-               
+
                 String pays = request.getParameter("pays");
-                
+
                 String telephone = request.getParameter("telephone");
-                
+
                 String fax = request.getParameter("fax");
-                
+
                 //new ClientEntity newclient = new ClientEntity(code,societe,contact,fonction,adresse,ville,region,code_postal,pays,telephone,fax);
-                
                 //MyDao.updateClient(old,newclient);
-                
-            }catch (Exception e) {
+            } catch (Exception e) {
                 out.printf("Erreur pendant la modif de données perso");
             }
 
-
 //ajouter des commandes// 
-
             try {
 
                 String datesend = request.getParameter("receptiondate");
 
                 String fee = request.getParameter("fee");
-                
+
                 String destination = request.getParameter("destination");
-                
-                String add_livraison  = request.getParameter("add_livraison");
-                
+
+                String add_livraison = request.getParameter("add_livraison");
+
                 String city_livraison = request.getParameter("city_livraison");
-                
-                String region_livraison  =request.getParameter("region_livraison");
-                
+
+                String region_livraison = request.getParameter("region_livraison");
+
                 String zipCode = request.getParameter("zipCode");
-                
+
                 String country_livraison = request.getParameter("country_livraison");
-                
+
                 String remise = request.getParameter("remise");
-                
+
                 //OrderEntity neworder = new OrderEntity(clientlogged,datesend,fee,destionation,add_livrasion,city_livrasion,region_livrasion,zipCode,country_livrasion,remise);   
-                
                 //myDao.addOrder(neworder);
-                
-                
-                
-                
                 //MyDao.addCommande(neworder);
-                
-            }catch (Exception e) {
+            } catch (Exception e) {
                 out.printf("erreur pendant l'ajout d'une commande");
             }
-    
 
-            
-
-
-
-            
 /////////////////////////////////////
-
-
 ////supprimer des commandes 
-            try{
+            try {
                 String num = request.getParameter("num");
-                
+
                 //myDao.deleteOrder(num);
-            
-            }catch (Exception e){
+            } catch (Exception e) {
                 out.printf("erreur lors de la suppression d'une commande");
             }
 
 /////////////////////////////////////
-
-
-
 ////editer des commandes
             try {
-                
+
                 String datesend = request.getParameter("receptiondate");
 
                 String fee = request.getParameter("fee");
-                
+
                 String destination = request.getParameter("destination");
-                
-                String add_livraison  = request.getParameter("add_livraison");
-                
+
+                String add_livraison = request.getParameter("add_livraison");
+
                 String city_livraison = request.getParameter("city_livraison");
-                
-                String region_livraison  =request.getParameter("region_livraison");
-                
+
+                String region_livraison = request.getParameter("region_livraison");
+
                 String zipCode = request.getParameter("zipCode");
-                
+
                 String country_livraison = request.getParameter("country_livraison");
-                
+
                 String remise = request.getParameter("remise");
-                
-                
-                
+
                 //OrderEntity neworder = new OrderEntity(clientlogged,datesend,fee,destionation,add_livrasion,city_livrasion,region_livrasion,zipCode,country_livrasion,remise);   
-                
                 //myDao.editOrder(oldorder,neworder);
-                
-            }catch( Exception e){
+            } catch (Exception e) {
                 out.printf("erreur lors de la modification d'une commande");
             }
 
-
-
 /////////////////////////////////////
-
-
-
-
-
-
         } catch (Exception ex) {
             Logger.getLogger("servlet").log(Level.SEVERE, "Erreur de traitement", ex);
         }
-        
+
     }
 }
