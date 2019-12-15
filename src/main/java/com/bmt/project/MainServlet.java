36fe6e64b2fefb15a6e0e36a9578529868d7d078
bruno <brunopierre.mzl@gmail.com>
2019-12-15 14:27:35 +0100

    [CTRL] on enleve les trucs qui serve a rien

--- src/main/java/com/bmt/project/MainServlet.java
+++ src/main/java/com/bmt/project/MainServlet.java
@@ -161,7 +161,7 @@ public class MainServlet extends HttpServlet {
                
 
 //              si c'est un client
-                if (client==true){
+                if (client){
                     List<OrderEntity> clientOrder = MyDao.getOrderListByClient(pass);
                     ArrayList<ArrayList<String>> clientOrderString = new ArrayList<ArrayList<String>>();
                     
@@ -236,107 +236,6 @@ public class MainServlet extends HttpServlet {
 
         request.getRequestDispatcher("/WEB-INF/products_presentation.jsp").forward(request, response);
 
-        try {
-
-// modifier les informations personnelles//
-            try {
-
-                //String code = clientlogged.getCode();
-                //String societe = clientlogged.getCompany();
-                String contact = request.getParameter("contact");
-
-                String fonction = request.getParameter("fonction");
-
-                String adresse = request.getParameter("adresse");
-
-                String ville = request.getParameter("ville");
-
-                String region = request.getParameter("region");
-
-                String code_postal = request.getParameter("code_postal");
-
-                String pays = request.getParameter("pays");
-
-                String telephone = request.getParameter("telephone");
-
-                String fax = request.getParameter("fax");
-
-                //new ClientEntity newclient = new ClientEntity(code,societe,contact,fonction,adresse,ville,region,code_postal,pays,telephone,fax);
-                //MyDao.updateClient(old,newclient);
-            } catch (Exception e) {
-                out.printf("Erreur pendant la modif de données perso");
-            }
-
-//ajouter des commandes// 
-            try {
-
-                String datesend = request.getParameter("receptiondate");
-
-                String fee = request.getParameter("fee");
-
-                String destination = request.getParameter("destination");
-
-                String add_livraison = request.getParameter("add_livraison");
-
-                String city_livraison = request.getParameter("city_livraison");
-
-                String region_livraison = request.getParameter("region_livraison");
-
-                String zipCode = request.getParameter("zipCode");
-
-                String country_livraison = request.getParameter("country_livraison");
-
-                String remise = request.getParameter("remise");
-
-                //OrderEntity neworder = new OrderEntity(clientlogged,datesend,fee,destionation,add_livrasion,city_livrasion,region_livrasion,zipCode,country_livrasion,remise);   
-                //myDao.addOrder(neworder);
-                //MyDao.addCommande(neworder);
-            } catch (Exception e) {
-                out.printf("erreur pendant l'ajout d'une commande");
-            }
-
-/////////////////////////////////////
-////supprimer des commandes 
-            try {
-                String num = request.getParameter("num");
-
-                //myDao.deleteOrder(num);
-            } catch (Exception e) {
-                out.printf("erreur lors de la suppression d'une commande");
-            }
-
-/////////////////////////////////////
-////editer des commandes
-            try {
-
-                String datesend = request.getParameter("receptiondate");
-
-                String fee = request.getParameter("fee");
-
-                String destination = request.getParameter("destination");
-
-                String add_livraison = request.getParameter("add_livraison");
-
-                String city_livraison = request.getParameter("city_livraison");
-
-                String region_livraison = request.getParameter("region_livraison");
-
-                String zipCode = request.getParameter("zipCode");
-
-                String country_livraison = request.getParameter("country_livraison");
-
-                String remise = request.getParameter("remise");
-
-                //OrderEntity neworder = new OrderEntity(clientlogged,datesend,fee,destionation,add_livrasion,city_livrasion,region_livrasion,zipCode,country_livrasion,remise);   
-                //myDao.editOrder(oldorder,neworder);
-            } catch (Exception e) {
-                out.printf("erreur lors de la modification d'une commande");
-            }
-
-/////////////////////////////////////
-        } catch (Exception ex) {
-            Logger.getLogger("servlet").log(Level.SEVERE, "Erreur de traitement", ex);
-        }
 
     }
 }
